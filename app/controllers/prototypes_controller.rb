aa52dab778345099c833c386fadc3e13fa8e8477
yryuz <81128880+yryuz@users.noreply.github.com>
2021-04-08 17:03:37 +0900

    修正１

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -27,7 +27,7 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
-    unless user_signed_in?
+    unless user_signed_in? && @prototype.user.id == current_user.id
       redirect_to action: :index
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @prototypes = @user.prototypes.includes(:user)
   end
 
 end
